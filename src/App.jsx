89c388ad1a8aae86bf0a7d988dfb92d7db19637c
nursultan <znslt24@gmail.com>
2025-03-26 13:23:00 +0300

    finished footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { createRoot } from "react-dom/client";
 import "./styles/global.scss";
 import { Header } from "./components/Header";
+import { Footer } from "./components/Footer";
 
 createRoot(document.getElementById("root")).render(
   <>
     <Header />
+
+    <Footer />
   </>
 );
