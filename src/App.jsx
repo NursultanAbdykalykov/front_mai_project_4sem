c4e7bceb805d6016b34cb37ce573d475d641ed57
nursultan <znslt24@gmail.com>
2025-03-26 14:57:52 +0300

    finished features component

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,12 @@ import { createRoot } from "react-dom/client";
 import "./styles/global.scss";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
+import { Features } from "./components/Features";
 
 createRoot(document.getElementById("root")).render(
   <>
     <Header />
-
+    <Features />
     <Footer />
   </>
 );
