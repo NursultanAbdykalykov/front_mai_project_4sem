789da4032c764d78b340d804075160e2dfa8ca02
nursultan <znslt24@gmail.com>
2025-03-26 13:06:38 +0300

    finished header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,29 @@
+import lightThemeIcon from "./lightMode.png";
+import styles from "./styles.module.scss";
+
+export const Header = () => {
+  const toggleTheme = () => {
+    console.log("Dark");
+  };
+  return (
+    <div className={styles.Header}>
+      <div className={styles.Logo}>
+        <p className={styles.headerTitle}>Has-Been</p>
+        <p className={styles.headerSubtitle}>Восстановление цвета</p>
+      </div>
+      <button
+        className={styles.themeSwitch}
+        onClick={toggleTheme}
+        aria-label="Сменить тему"
+      >
+        <img
+          src={lightThemeIcon}
+          alt="Иконка темы"
+          className={styles.themeIcon}
+          width={48}
+          height={48}
+        />
+      </button>
+    </div>
+  );
+};
