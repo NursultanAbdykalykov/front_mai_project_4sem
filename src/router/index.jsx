9d2a8fdd8761e93604d5cdae9fc84a640572bc99
nursultan <znslt24@gmail.com>
2025-04-15 15:37:42 +0300

    FRONT-8: added react-router

---
+++ src/router/index.jsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import { App } from "../App";
+import { Home } from "../pages/Home";
+import { Result } from "../pages/Result";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: "result", element: <Result /> },
+    ],
+  },
+]);
