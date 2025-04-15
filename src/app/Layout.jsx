293d5050f495d4cf47111531787ac837e2113eff
nursultan <znslt24@gmail.com>
2025-04-15 16:20:47 +0300

    refactor: add Layout component for common structure

--- src/app/Layout.jsx
+++ src/app/Layout.jsx
@@ -4,7 +4,7 @@ import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import "../styles/global.scss";
 
-export const App = () => {
+export const Layout = () => {
   return (
     <div className="app">
       <Header />
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,12 @@
 import { createBrowserRouter } from "react-router-dom";
-import { App } from "../app/App";
+import { Layout } from "../app/Layout";
 import { Home } from "../pages/Home";
 import { Result } from "../pages/Result";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />,
+    element: <Layout />,
     children: [
       { index: true, element: <Home /> },
       { path: "result", element: <Result /> },
