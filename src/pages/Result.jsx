96d6b223e4f76fb34bcfbf7d3b572e9c9f2fc3e7
nursultan <znslt24@gmail.com>
2025-04-19 19:09:12 +0300

    FRONT-14: add photocard to result page

--- src/pages/Result.jsx
+++ src/pages/Result.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styles from "./styles.module.scss";
 import photoIconUrl from "../shared/assets/icons/photoIcon.svg";
+import { PhotoCard } from "../components/PhotoCard";
 
 export const Result = () => {
   return (
@@ -13,7 +14,17 @@ export const Result = () => {
         <p>Заварите чай пока наш исскуственный интеллект</p>
         <p>обрабатывает ваш запрос</p>
       </div>
-      <div className={styles.resultContainer}></div>
+      <div className={styles.resultContainer}>
+        <div className={styles.downloadInformation}></div>
+        <div className={styles.photosContainer}>
+          <PhotoCard width={250} height={250} />
+          <PhotoCard width={150} height={150} />
+          <PhotoCard width={150} height={150} />
+          <PhotoCard width={150} height={150} />
+          <PhotoCard width={150} height={150} />
+          <PhotoCard width={150} height={150} />
+        </div>
+      </div>
     </div>
   );
 };
